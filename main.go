2ff3321905c6a7d364aae85153b847be1faead92
guam <56258709+guamencja@users.noreply.github.com>
2025-09-12 19:08:54 +0200

    send, (un)pin -msg impl & msgId in temp file
    so i dont have to send a message manually to get a messageid to put in the config

--- main.go
+++ main.go
@@ -2,9 +2,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 	"strconv"
 	"time"
 
@@ -13,10 +15,9 @@ import (
 )
 
 type config struct {
-	Token     string
-	Cooldown  int
-	ChatId    string
-	MessageId string
+	Token    string
+	Cooldown int
+	ChatId   string
 }
 
 func getConfig() config {
@@ -27,11 +28,49 @@ func getConfig() config {
 	cooldown, _ := strconv.Atoi(os.Getenv("COOLDOWN"))
 
 	return config{
-		Token:     os.Getenv("TOKEN"),
-		Cooldown:  cooldown,
-		ChatId:    os.Getenv("CHAT_ID"),
-		MessageId: os.Getenv("MESSAGE_ID"),
+		Token:    os.Getenv("TOKEN"),
+		Cooldown: cooldown,
+		ChatId:   os.Getenv("CHAT_ID"),
 	}
+}
+
+type temp struct {
+	MessageId string `json:"message_id"`
+}
+
+const tempFile = "temp.json"
+
+func getTempFile() (temp, error) {
+	var t temp
+
+	// ensure file exists
+	if _, err := os.Stat(tempFile); os.IsNotExist(err) {
+		return t, nil // return empty
+	}
+
+	data, err := os.ReadFile(tempFile)
+	if err != nil {
+		return t, err
+	}
+
+	if err := json.Unmarshal(data, &t); err != nil {
+		return t, err
+	}
+
+	return t, nil
+}
+
+func saveTempFile(t temp) error {
+	if err := os.MkdirAll(filepath.Dir(tempFile), 0755); err != nil {
+		return err
+	}
+
+	data, err := json.MarshalIndent(t, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(tempFile, data, 0644)
 }
 
 func main() {
@@ -45,6 +84,31 @@ func main() {
 	}
 	log.Printf("Logged in as @%s (%d)", user.Username, user.Id)
 
+	// CHECK FOR MESSAGE ID
+
+	// load temp file
+	t, err := getTempFile()
+	if err != nil {
+		log.Fatalf("failed to read temp file: %v", err)
+	}
+
+	if t.MessageId == "" {
+		id, err := bot.SendMessageText(config.ChatId, "loading...")
+		if err != nil {
+			log.Println(err)
+		}
+
+		if err := bot.PinMessage(config.ChatId, id); err != nil {
+			log.Println(err)
+		}
+
+		t.MessageId = id
+		if err := saveTempFile(t); err != nil {
+			log.Println("failed to save temp file:", err)
+		}
+	}
+
+	// COUNTDOWN
 	summer := getSummer()
 	for range time.Tick(time.Second * time.Duration(config.Cooldown)) {
 		d := getTimeRemaining(summer)
@@ -59,7 +123,7 @@ func main() {
 		}
 
 		text := fmt.Sprintf("%d dni, %d godz, %d min i %d sek do %s", d.Days, d.Hours, d.Minutes, d.Seconds, str)
-		if err := bot.EditMessageText(config.ChatId, config.MessageId, text); err != nil {
+		if err := bot.EditMessageText(config.ChatId, t.MessageId, text); err != nil {
 			log.Println(err)
 		}
 	}
--- telegram/methods.go
+++ telegram/methods.go
@@ -1,5 +1,7 @@
 package telegram
 
+import "fmt"
+
 type User struct {
 	Id           int    `json:"id"`
 	IsBot        bool   `json:"is_bot"`
@@ -27,6 +29,45 @@ func (b *Bot) EditMessageText(chatId, messageId, content string) error {
 		"text":       content,
 	}
 	if err := b.request("editMessageText", data, nil); err != nil {
+		return err
+	}
+	return nil
+}
+
+// sendMessage
+func (b *Bot) SendMessageText(chatId, content string) (string, error) { // returns messageId if succeded along with a possible error
+	data := map[string]interface{}{
+		"chat_id": chatId,
+		"text":    content,
+	}
+	r := map[string]interface{}{}
+	if err := b.request("sendMessage", data, &r); err != nil {
+		return "", err
+	}
+	msgId := fmt.Sprintf("%v", r["message_id"])
+	return msgId, nil
+}
+
+// pinChatMessage
+func (b *Bot) PinMessage(chatId, messageId string) error {
+	data := map[string]interface{}{
+		"chat_id":              chatId,
+		"message_id":           messageId,
+		"disable_notification": true,
+	}
+	if err := b.request("pinChatMessage", data, nil); err != nil {
+		return err
+	}
+	return nil
+}
+
+// unpinChatMessage
+func (b *Bot) UnpinMessage(chatId, messageId string) error {
+	data := map[string]interface{}{
+		"chat_id":    chatId,
+		"message_id": messageId,
+	}
+	if err := b.request("unpinChatMessage", data, nil); err != nil {
 		return err
 	}
 	return nil
